f52b16a87f1ccabaede0e058c9feadfc38816f82
Fmanzi <fmanzi350@gmail.com>
2023-03-17 11:27:35 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+i#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * puts: it print to screen..
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
