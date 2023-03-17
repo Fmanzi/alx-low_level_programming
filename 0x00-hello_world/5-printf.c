fa05d346c556bcbf0dfd147bbafec82a5f2afcfc
Fmanzi <fmanzi350@gmail.com>
2023-03-17 11:31:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main Entry
+ *
+ * Printf: it prints out
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
