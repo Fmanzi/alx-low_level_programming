c2369046a79568b8db41f577d255c2320ef7d6fc
Fmanzi <fmanzi350@gmail.com>
2023-03-20 15:59:40 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,7 +6,6 @@
  *
  * puts: it print to screen..
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
