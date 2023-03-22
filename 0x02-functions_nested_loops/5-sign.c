fb8564485379698a90d4b1814db061cc2004ea43
Fmanzi <fmanzi350@gmail.com>
2023-03-21 19:49:54 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that print the sign of a number
+ *
+ * @n: it takes the input of the function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+}
