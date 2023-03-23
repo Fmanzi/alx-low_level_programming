9a20c60d2d93b77cf1fbf12657f7a5856b9ad4eb
Fmanzi <fmanzi350@gmail.com>
2023-03-22 19:25:43 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - it add two numbers
+ *
+ * @a: the input value
+ *
+ * @b: the input value
+ *
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
