6f4a44ee52d1872450fe8972fc8098ec4b723108
Fmanzi <fmanzi350@gmail.com>
2023-03-23 18:14:29 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - it checks for uppercase alphabet
+ *
+ * @c: the input value of the _isupper function
+ *
+ * Return: Always 0
+ *
+ */
+
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
