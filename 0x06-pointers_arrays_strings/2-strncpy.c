d848f06738b2bb5d1d8c7d8b97a83e26fa102831
Fmanzi <fmanzi350@gmail.com>
2023-03-31 17:37:01 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,47 +1,18 @@
-#include <stdio.h>
-
+/**
+ * _strncpy - Copies the first n bytes of the src string to dest
+ * @dest: Pointer to the destination buffer
+ * @src: Pointer to the source string
+ * @n: Number of bytes to copy
+ *
+ * Return: Pointer to the destination buffer
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
+
 for (i = 0; i < n && src[i] != '\0'; i++)
-{
 dest[i] = src[i];
-}
-for (; i < n; i++)
-{
+for ( ; i < n; i++)
 dest[i] = '\0';
-}
 return (dest);
-}
-int main(void)
-{
-char s1[98];
-char *ptr;
-int i;
-for (i = 0; i < 98 - 1; i++)
-{
-s1[i] = '*';
-}
-s1[i] = '\0';
-printf("%s\n", s1);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-printf("%s\n", s1);
-printf("%s\n", ptr);
-ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-printf("%s", s1);
-printf("%s", ptr);
-for (i = 0; i < 98; i++)
-{
-if (i % 10)
-{
-printf(" ");
-}
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", s1[i]);
-}
-printf("\n");
-return (0);
 }
