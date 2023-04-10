749f05caffdec15cd10029917eb73d31364fa371
Fmanzi <fmanzi350@gmail.com>
2023-04-10 16:46:21 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  *@argv: array of input strings
  *x: string
  *y: string
- * Return: 0
+ * Return: 0 on succes, 1 error
  */
 int main(int argc, char *argv[])
 {
@@ -17,10 +17,8 @@ if (argc != 3)
 printf("error\n");
 return (1);
 }
-
 x = atoi(argv[1]);
 y = atoi(argv[2]);
-
 printf("%d\n", x *y);
 return (0);
 }
