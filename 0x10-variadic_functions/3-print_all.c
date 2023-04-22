6814bf5c2a32372665801b81b20eb417e8281ee5
Fmanzi <fmanzi350@gmail.com>
2023-04-21 17:49:04 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,26 +1,23 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
- *print_all - function call
- *@format: placeholder variable
- *
+ *print_all - Function to print
+ *@format: contains list of args
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
-va_start(args, format);
-unsigned int i = 0;
-char c;
-int n;
-float f;
+int i = 0;
 char *s;
-while (format != NULL && format[i] != '\0')
+char c;
+int ival;
+float fval;
+
+va_start(args, format);
+while (format && format[i])
 {
-if (i > 0)
-{
-printf(", ");
-}
 switch (format[i])
 {
 case 'c':
@@ -28,29 +25,26 @@ c = va_arg(args, int);
 printf("%c", c);
 break;
 case 'i':
-n = va_arg(args, int);
-printf("%d", n);
+ival = va_arg(args, int);
+printf("%d", ival);
 break;
 case 'f':
-f = va_arg(args, double);
-printf("%f", f);
+fval = va_arg(args, double);
+printf("%f", fval);
 break;
 case 's':
 s = va_arg(args, char *);
-if (s != NULL){
+if (s)
 printf("%s", s);
-}
 else
-{
 printf("(nil)");
-}
+break;
+default:
 break;
 }
+if (format[i + 1])
+printf(", ");
 i++;
-while (format[i] != '\0' && format[i] != 'c' && format[i] != 'i' && format[i] != 'f' && format[i] != 's')
-{
-i++;
-}
 }
 va_end(args);
 printf("\n");
