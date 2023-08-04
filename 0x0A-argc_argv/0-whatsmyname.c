f29b3967235a971c959ac2541ba821faaa03531e
Fmanzi <fmanzi350@gmail.com>
2023-08-04 13:59:46 -0700

    Task 0, argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - The Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s \n", argv[0]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/_puts.c
@@ -0,0 +1,13 @@
+/**
+ * _puts - Prints a string to stdout
+ * @str: The string to be printed
+ */
+void _puts(char *str)
+{
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+}
